bda574c445f723d28d7ba4a8e303ff9f6a4e9bd4
Jordan Harrison <jordandh@gmail.com>
2014-05-02 16:30:38 -0700

    Added the ability to define mutliple routes for nav-tabs.

--- src/nav-tabs.js
+++ src/nav-tabs.js
@@ -2,39 +2,46 @@
   * nav-tabs
   */
 define(['sprout/dom', 'sprout/util', 'sprout/router'], function ($, _, router) {
-	'use strict';
+    'use strict';
 
-	$(function () {
-		$('.nav-tabs, .nav-pills').each(function () {
-			var menu = $(this),
-				routes = {};
+    $(function () {
+        $('.nav-tabs, .nav-pills').each(function () {
+            var menu = $(this),
+                routes = {};
 
             menu.find('[data-url]').each(function () {
-				var item = $(this),
-					url = item.attr('data-url');
+                var item = $(this),
+                    url = item.attr('data-url'),
+                    urls = item.attr('data-routes');
 
-                routes[_.uniqueId('nav-tabs.') + '.' + url] = {
-                    path: url,
-                    start: function () {
-						item.tab('show');
-                    }
-                };
+                urls = _.isString(urls) ? urls.split(',') : [url];
+
+                _.each(urls, function (routeUrl) {
+                    routeUrl = _.trim(routeUrl);
+
+                    routes[_.uniqueId('nav-tabs.') + '.' + routeUrl] = {
+                        path: routeUrl,
+                        start: function () {
+                            item.tab('show');
+                        }
+                    };
+                });
             });
 
             // Default url for the naked page url
             menu.find('[data-default-url]').each(function () {
-				var item = $(this),
-					url = item.attr('data-url') || '';
+                var item = $(this),
+                    url = item.attr('data-url') || '';
 
                 routes[_.uniqueId('nav-tabs.') + (url ? '.' : '') + url + '.default-url'] = {
                     path: '',
                     start: function () {
-						item.tab('show');
+                        item.tab('show');
                     }
                 };
             });
 
             router.defaultRouter.add(routes);
-		});
-	});
+        });
+    });
 });
