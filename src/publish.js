e6406bdeb31b1c957ba87a939b94feef26a5794e
Jordan Harrison <jordandh@gmail.com>
2015-08-09 12:16:30 -0700

    Added the publish's selector and handler function to the dom module so that it can be reused by other code. This can be necessary if the click event does not bubble up to the body.

--- src/publish.js
+++ src/publish.js
@@ -4,32 +4,36 @@
 define(['sprout/util', 'sprout/dom', 'sprout/pubsub'], function (_, $, pubsub) {
 	'use strict';
 
-	$(function () {
-		$('body').on('click.publish.data-api', '[data-publish]', function (event) {
-			var $this = $(this),
-				message = $this.attr('data-publish'),
-				info = {};
+	$.onPublishClicked = function (event) {
+		var $this = $(this),
+			message = $this.attr('data-publish'),
+			info = {};
 
-			if (message === "" || $this.is('.disabled, :disabled, [data-publish-disabled]') || !_.isUndefined($(event.target).attr('data-publish-prevent'))) {
-				return;
-			}
+		if (message === "" || $this.is('.disabled, :disabled, [data-publish-disabled]') || !_.isUndefined($(event.target).attr('data-publish-prevent'))) {
+			return;
+		}
 
-			if (_.isUndefined($this.attr('data-publish-default'))) {
-				event.preventDefault();
-			}
+		if (_.isUndefined($this.attr('data-publish-default'))) {
+			event.preventDefault();
+		}
 
-			_.each($this.attrs(), function (value, name) {
-				if (_.startsWith(name, 'data-publish-')) {
-					info[_.strRight(name, 'data-publish-')] = value;
-				}
-			});
-
-			pubsub.publish(message, info, this);
-
-			// If the message should only be published once then remove the data-publish attribute to prevent another message from being published
-			if (!_.isUndefined($this.attr('data-publish-once'))) {
-				$this.removeAttr('data-publish');
+		_.each($this.attrs(), function (value, name) {
+			if (_.startsWith(name, 'data-publish-')) {
+				info[_.strRight(name, 'data-publish-')] = value;
 			}
 		});
+
+		pubsub.publish(message, info, this);
+
+		// If the message should only be published once then remove the data-publish attribute to prevent another message from being published
+		if (!_.isUndefined($this.attr('data-publish-once'))) {
+			$this.removeAttr('data-publish');
+		}
+	};
+
+	$.onPublishClickedSelector = '[data-publish]';
+
+	$(function () {
+		$('body').on('click.publish.data-api', $.onPublishClickedSelector, $.onPublishClicked);
 	});
 });
